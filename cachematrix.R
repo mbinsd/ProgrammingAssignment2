fc8729e42d9e36212d9d8d1e2d7f5bb1c430e5e5
mbinsd <mbinsd@gmail.com>
2014-07-25 16:21:54 -0700

    Turning in Assignment
    Added R code to template

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+###Programming Assignment 2
+#Data Science - John Hopkins
+#Mark Blackman
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix<-function(m){
+  mi<-NULL
+  #### Create or "set" Matrix  
+  set<-function(y){
+    m<<-y
+    mi<<-NULL
+  } 
+  #### Retrieve or "get" matrix  
+  get<-function() m
+  #### Cache the inverse of m or the set matrix
+  setmi<-function(matrixinverse) mi <<- matrixinverse
+  ###Retrieve or "get" the cached matrix inver
+  getmi<-function() mi
+  list(set = set, get = get,setmi = setmi, getmi=getmi)
 }
+#####Use makeCacheMatrix to see if there's cached version of the inverse matrix
+#if not solve(matrix) and cache
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve<-function(m, ...){
+  mi<-m$getmi()
+  if(!is.null(mi)) {
+    message("getting cached inverse matrix")
+    return(mi)
+  }
+  data<-m$get()
+  mi<-solve(data)
+  m$setmi(mi)
+  mi
 }
